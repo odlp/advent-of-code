526cff184b9d0870225d90efbd267a8c9e327d6f
Oliver Peate <oli@shippedcode.com>
2020-12-02 14:10:18 +0000

    Day 2: Simplify

--- day-2/passwords.rb
+++ day-2/passwords.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/env ruby
 
-challenge_part = ARGV.first.to_i
 input_path = File.expand_path("passwords.txt", __dir__)
-
 PasswordEntry = Struct.new(:character, :range, :password, keyword_init: true)
 
 password_entries = File.readlines(input_path).map do |line|
@@ -13,25 +11,15 @@ password_entries = File.readlines(input_path).map do |line|
   PasswordEntry.new(character: character, range: start..finish, password: password)
 end
 
-# Part 1
-min_max_occurrences_strategy = lambda do |entry|
+answer_part_1 = password_entries.count do |entry|
   entry.range.include?(entry.password.count(entry.character))
 end
 
-# Part 2
-one_occurrence_at_positions_strategy = lambda do |entry|
+answer_part_2 = password_entries.count do |entry|
   [entry.range.begin, entry.range.end].one? do |position|
     index = position - 1
     entry.password[index] == entry.character
   end
 end
 
-strategy = if challenge_part == 1
-  min_max_occurrences_strategy
-else
-  one_occurrence_at_positions_strategy
-end
-
-valid_count = password_entries.count(&strategy)
-
-puts "Valid passwords: #{valid_count}"
+puts "Part 1: #{answer_part_1}", "Part 2: #{answer_part_2}"
