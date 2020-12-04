31f14f6d3e512c575bdf96bd86d01b042e9988d6
Oliver Peate <oli@shippedcode.com>
2020-12-04 12:19:38 +0000

    Day 4: Compact

--- day-4/passports.rb
+++ day-4/passports.rb
@@ -1,39 +1,23 @@
-require "pry"
+#!/usr/bin/env ruby
 
-# Optional: cid (Country ID)
-
-required_fields = [
-  "byr", # (Birth Year)
-  "iyr", # (Issue Year)
-  "eyr", # (Expiration Year)
-  "hgt", # (Height)
-  "hcl", # (Hair Color)
-  "ecl", # (Eye Color)
-  "pid", # (Passport ID)
-]
+REQUIRED_FIELDS = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+VALID_EYE_COLORS = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
 
 def valid_field?(field, value)
   case field
-  when "byr"
-    value.to_i.between?(1920, 2002)
-  when "iyr"
-    value.to_i.between?(2010, 2020)
-  when "eyr"
-    value.to_i.between?(2020, 2030)
+  when "byr" then value.to_i.between?(1920, 2002)
+  when "iyr" then value.to_i.between?(2010, 2020)
+  when "eyr" then value.to_i.between?(2020, 2030)
+  when "hcl" then value.match?(/\A#[0-9a-f]{6}\z/)
+  when "ecl" then VALID_EYE_COLORS.include?(value)
+  when "pid" then value.match?(/\A\d{9}\z/)
+  when "cid" then true
   when "hgt"
     if value.include?("cm")
       value.to_i.between?(150, 193)
     elsif value.include?("in")
       value.to_i.between?(59, 76)
     end
-  when "hcl"
-    value.match?(/\A#[0-9a-f]{6}\z/)
-  when "ecl"
-    ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].include?(value)
-  when "pid"
-    value.match?(/\A\d{9}\z/)
-  when "cid"
-    true
   end
 end
 
@@ -42,16 +26,15 @@ passports = ARGF
   .map { |lines| lines.join.gsub("\n", " ").strip }
 
 passports_with_required_fields = passports.select do |passport|
-  required_fields.all? { |field| passport.include?(field) }
+  REQUIRED_FIELDS.all? { |field| passport.include?(field) }
 end
 
-puts "Part 1:", passports_with_required_fields.size
-
-part_2_count = passports_with_required_fields.count do |passport|
+required_and_valid_count = passports_with_required_fields.count do |passport|
   passport.split(" ").all? do |field_value|
     field, value = field_value.split(":")
     valid_field?(field, value)
   end
 end
 
-puts "Part 2:", part_2_count
+puts "Part 1:", passports_with_required_fields.count
+puts "Part 2:", required_and_valid_count
