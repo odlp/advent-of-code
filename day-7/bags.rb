6e896527723a238674f60f2d86f500c35a861127
Oliver Peate <oli@shippedcode.com>
2020-12-07 14:49:13 +0000

    Day 7: Part 2

--- day-7/bags.rb
+++ day-7/bags.rb
@@ -1,32 +1,37 @@
 require "pry"
 
-RULES = ARGF.map do |raw_rule|
+RULES = ARGF.each_with_object({}) do |raw_rule, result|
   outer_bag, raw_inner_bags = raw_rule.split(" bags contain ")
-  inner_bags = {}
-  raw_inner_bags.scan(/(\d+)\s([\w\s]+)\sbag/).each do |qty, color|
-    inner_bags[color] = qty.to_i
-  end
+  inner_bags = raw_inner_bags
+    .scan(/(\d+)\s([\w\s]+)\sbag/)
+    .map { |qty, color| [color, qty.to_i] }.to_h
 
-  [outer_bag, inner_bags]
-end.to_h
+  result[outer_bag] = inner_bags
+end
 
 def find_outer_bags_for(color)
-  RULES.filter do |outer, inner_bags|
-    inner_bags.key?(color)
-  end.keys
+  RULES.filter { |_, inner_bags| inner_bags.key?(color) }.keys
 end
 
 def distinct_outer_bags_for(color)
-  answers = []
   search_list = [color]
+  outer_bags = []
 
-  while(next_color = search_list.pop)
+  while (next_color = search_list.pop)
     bags = find_outer_bags_for(next_color)
     search_list += bags
-    answers += bags
+    outer_bags += bags
   end
 
-  answers.uniq.size
+  outer_bags.uniq.size
+end
+
+def inner_bags_quantities_for(color)
+  RULES[color].inject(1) do |answer, (inner_color, quantity)|
+    inner_quantity = inner_bags_quantities_for(inner_color)
+    answer + (quantity * inner_quantity)
+  end
 end
 
 puts "Part 1:", distinct_outer_bags_for("shiny gold")
+puts "Part 2:", inner_bags_quantities_for("shiny gold") - 1
