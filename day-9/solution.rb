eaf1fb32e076e5ebe94e66374046cdb89ae5645f
Oliver Peate <oli@shippedcode.com>
2020-12-09 10:02:46 +0000

    Day 9: Part 2

--- day-9/solution.rb
+++ day-9/solution.rb
@@ -10,3 +10,14 @@ invalid = sequence[preamble..-1].detect.with_index(preamble) do |number, index|
 end
 
 puts "Part 1:", invalid
+
+pre_invalid = Range.new(0, sequence.index(invalid) - 1)
+
+1.upto(pre_invalid.size).each do |n_consecutive|
+  sequence[pre_invalid].each_cons(n_consecutive) do |values|
+    if values.sum == invalid
+      puts "Part 2:", values.minmax.sum
+      return
+    end
+  end
+end
