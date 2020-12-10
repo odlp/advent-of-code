4106dafcba9df1486116a3a5e22220f2413e2e1a
Oliver Peate <oli@shippedcode.com>
2020-12-10 15:01:14 +0000

    Day 10: Part 2

--- day-10/solution.rb
+++ day-10/solution.rb
@@ -1,5 +1,6 @@
 adapters = ARGF.map(&:to_i).sort
-chain = [0, *adapters, adapters.last + 3]
+SOCKET = 0
+chain = [SOCKET, *adapters, adapters.last + 3]
 
 differences = chain
   .each_cons(2)
@@ -7,3 +8,16 @@ differences = chain
   .tally
 
 puts "Part 1:", differences, differences.values.reduce(:*)
+
+def calculate_permutations(n:, values:, cache:)
+  return 0 unless values.include?(n)
+  return 1 if n == SOCKET
+
+  cache[n] ||= begin
+    1.upto(3).sum do |offset|
+      calculate_permutations(n: n - offset, values: values, cache: cache)
+    end
+  end
+end
+
+puts "Part 2:", calculate_permutations(n: chain.last, values: chain, cache: {})
