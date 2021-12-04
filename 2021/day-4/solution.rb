51751b52eb8a28813d925d295ea9fc3a9b896178
Oliver Peate <oliverp@gmail.com>
2021-12-04 18:05:20 +0000

    Day 4 refactoring

--- 2021/day-4/solution.rb
+++ 2021/day-4/solution.rb
@@ -2,14 +2,15 @@
 
 require "matrix"
 
-Cell = Struct.new(:value, :marked, keyword_init: true)
+Cell = Struct.new(:value, :marked, keyword_init: true) do
+  def mark_match(number)
+    self.marked = true if value == number
+  end
+end
 
 def find_winners(boards)
   boards.select do |board|
-    row_win = board.row_vectors.any? { |row| row.all?(&:marked) }
-    column_win = board.column_vectors.any? { |column| column.all?(&:marked) }
-
-    row_win || column_win
+    (board.row_vectors + board.column_vectors).any? { |sequence| sequence.all?(&:marked) }
   end
 end
 
@@ -18,41 +19,37 @@ def calculate_score(board, last_drawn_number)
   unmarked_sum * last_drawn_number
 end
 
-def mark_matching_numbers(boards, drawn_number)
+def mark_matches(drawn_number, boards)
   boards.each do |board|
-    board.each do |cell|
-      if cell.value == drawn_number
-        cell.marked = true
-      end
-    end
+    board.each { |cell| cell.mark_match(drawn_number) }
   end
 end
 
-def find_first_winner(boards, bingo_numbers)
+def find_first_winning_score(boards, bingo_numbers)
   boards = boards.dup
   bingo_numbers = bingo_numbers.dup
 
-  until winner = find_winners(boards).first || bingo_numbers.empty?
+  until winner = find_winners(boards).first
     drawn_number = bingo_numbers.shift
-    mark_matching_numbers(boards, drawn_number)
+    mark_matches(drawn_number, boards)
   end
 
-  [winner, drawn_number]
+  calculate_score(winner, drawn_number)
 end
 
-def find_last_winner(boards, bingo_numbers)
+def find_last_winning_score(boards, bingo_numbers)
   boards = boards.dup
   bingo_numbers = bingo_numbers.dup
   winners = []
 
   until bingo_numbers.empty? || boards.empty?
     drawn_number = bingo_numbers.shift
-    mark_matching_numbers(boards, drawn_number)
+    mark_matches(drawn_number, boards)
     winners += find_winners(boards)
     boards -= winners
   end
 
-  [winners.last, drawn_number]
+  calculate_score(winners.last, drawn_number)
 end
 
 def parse_boards(raw_boards)
@@ -69,5 +66,5 @@ raw_numbers, *raw_boards = ARGF.read.split("\n\n")
 bingo_numbers = raw_numbers.split(",").map(&:to_i)
 boards = parse_boards(raw_boards)
 
-puts "Part 1:", calculate_score(*find_first_winner(boards, bingo_numbers))
-puts "Part 2:", calculate_score(*find_last_winner(boards, bingo_numbers))
+puts "Part 1:", find_first_winning_score(boards, bingo_numbers)
+puts "Part 2:", find_last_winning_score(boards, bingo_numbers)
